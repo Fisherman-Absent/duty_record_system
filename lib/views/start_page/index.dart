3926e847be48a066b571e7e43422d8a7b3353515
Harrykuo1 <stu970157@gmail.com>
2023-11-29 00:40:29 +0800

    Simulate the start page jump Use automatic jump after three seconds to simulate

--- lib/views/start_page/index.dart
+++ lib/views/start_page/index.dart
@@ -2,8 +2,13 @@ import 'package:flutter/material.dart';
 
 class StartPage extends StatelessWidget {
   const StartPage({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
+    Future.delayed(Duration(seconds: 3),(){
+      Navigator.pushReplacementNamed(context, "/sign");  
+    });
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Start Page"),
