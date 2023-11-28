6dd747b07e6f2e6c9680edc54d73004a1fa4306d
Harrykuo1 <stu970157@gmail.com>
2023-11-29 04:33:29 +0800

    Add navbar and WorkModeSwitch to the page

--- lib/views/sign_page/index.dart
+++ lib/views/sign_page/index.dart
@@ -1,14 +1,39 @@
 import 'package:flutter/material.dart';
 
+import '../../style/colors.dart';
+import '../../components/bottomNav.dart';
+import './components/workModeSwitch.dart';
+
 class SignPage extends StatelessWidget {
   const SignPage({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Sign Page"),
+        backgroundColor: appBarBGColor,
         centerTitle: true,
+      ),
+      body:SignBody(),
+      bottomNavigationBar: BottomNavComponent(
+        selectedIndex: 0, 
       ),
     );
   }
 }
+
+class SignBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(  
+        children: [
+          Expanded(
+            flex: 20, 
+            child: WorkModeSwitch(),
+          ),
+        ],
+      );
+  }
+}
+
