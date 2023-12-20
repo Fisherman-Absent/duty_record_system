17851a446687c6bade651a416469e3e7bd50d012
jamessumac <joan3825ms55@gmail.com>
2023-12-20 14:08:53 +0800

    setting testing

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:pref/pref.dart';
 import './routers/routers.dart';
 
-void main() {
-  runApp(MaterialApp(
-    initialRoute: '/',
-    routes: getRoutes(),
-  )); 
+import 'package:upgrader/upgrader.dart';
+
+void main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final service = await PrefServiceShared.init(
+    defaults: {
+      'start_page': 'posts',
+      'ui_theme': 'light',
+    },
+  );
+  await Upgrader.clearSavedSettings();
+
+
+  runApp(
+    PrefService(
+      child: 
+        MaterialApp(
+          initialRoute: '/',
+          routes: getRoutes(),
+        ),
+      service: service
+      )
+    ); 
 }
